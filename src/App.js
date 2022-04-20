934d8ec089b8f2fd695fc5b34fb53b9f2006dd15
Juan Morales <juanmr2311@gmail.com>
2022-04-20 00:22:50 -0500

    Se revierten cambios en la clase app

--- src/App.js
+++ src/App.js
@@ -13,19 +13,13 @@ import Viscerocraneo from "./components/Viscerocraneo";
 
 
 function App() {
-
-  /*
-            <Route path="/:part" element={<Bones />} exact/>
-                      <Route path="/neurocraneo/:bone" element={<Neurocraneo />} exact/>
-
-  */
   return (
     <div className="App">
       <div>
       <Router>
         <Routes>
-          <Route path="/neurocraneo/frontal" element={<Frontal />} exact/>
-
+          <Route path="/:part" element={<Bones />} exact/>
+          <Route path="/neurocraneo/:bone" element={<Neurocraneo />} exact/>
           <Route path="/viscerocraneo/:bone" element={<Viscerocraneo />} exact/>
           <Route path="/" element={<Index />} />          
         </Routes>
